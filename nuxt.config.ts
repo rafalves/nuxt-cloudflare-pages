6430851eec771a4e25284dfe4bcf0f8f19c282a5
Rafael Alves <rafalves.costa@gmail.com>
2024-08-15 10:25:15 -0300

    commit 6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxthub/core", "@nuxtjs/tailwindcss"],
-  hub: {
-    database: true,
-    remote: 'production'
-  }
+  modules: ["@nuxtjs/tailwindcss", "nitro-cloudflare-dev"],
+
 })
